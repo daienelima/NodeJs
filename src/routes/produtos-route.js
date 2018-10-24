97d0716500658321376846ffc8b2eece021531f0
daienelima <daiene.m17@gmai.com>
2018-10-24 20:24:16 -0300

    controllers e mongoDB

--- src/routes/produtos-route.js
+++ src/routes/produtos-route.js
@@ -2,28 +2,10 @@
 
 const express = require('express');
 const router = express.Router();
+const controller = require('../controllers/produto-controller');
 
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: "Node store API",
-        version: "0.0.1"
-    });
-});
-
-router.post('/', (req, res, next) => {
-    res.status(201).send(req.body);
-});
-
-router.put('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id,
-        item: req.body
-    });
-});
-
-router.delete('/', (req, res, next) => {
-    res.status(200).send(req.body);
-});
+router.post('/', controller.post);
+router.put('/:id', controller.put);
+router.delete('/', controller.delete);
 
 module.exports = router;
