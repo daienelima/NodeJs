e9850e5380166ffe3318d3cf546b4a7e9fbc0f8b
Lima <daiene.m.lima@accenture.com>
2018-10-25 16:29:51 -0300

    Conection com db com erro

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
+//Connecta ao banco
+mongoose.connect('mongodb://daiene:daiene01@ds041167.mlab.com:41167/node_str');
 
 // Carregar as Rotas
 const indexRoute = require('./routes/index-route');
@@ -11,7 +14,6 @@ const produtoRoute = require('./routes/produtos-route');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 
 app.use('/', indexRoute);
 app.use('/produto', produtoRoute);
